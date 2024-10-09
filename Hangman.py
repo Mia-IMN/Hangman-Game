a1e73c0671603c57d2f0be861ef808ae44700745
Mia Ikechukwu <87150395+Mia-IMN@users.noreply.github.com>
2024-10-09 18:50:22 +0100

    Integrated final score
    The game can now track user scores from various attempts and calculate the final score.

--- Hangman.py
+++ Hangman.py
@@ -2,134 +2,191 @@ import random
 from Hangman_art import welcome_image, HANGMANPICS
 from Hangman_wordlist import easy_word_list, medium_word_list, hard_word_list
 
-# work on making a list of all the user input so you can check if they've inputted the letter before
-print(welcome_image)
+final_score = 0
 
+def play_game():
 
-# code that let's player choose difficulty level
-level_difficulty = input("Choose difficulty level: Easy || Medium || Hard\n")
+    # scoring system - each try would minus 10 marks out of 100 for easy, 150 for medium and 200 for hard
+    # fix easy not starting the game
+    print(welcome_image)
 
-# show_update is the array that contains the users guesses
-# this function reduces ambiguity caused by constant printing of
-# the show_update array in different ways.
-def printshow_update(number, functn, text = "What could this word be: "):
-    print(text, end="")
-    for num in range(number):
-        print(functn[num], end=" ")
-    
-# Play function
-def play(level):
+    # code that let's player choose difficulty level
+    level_difficulty = input("Choose difficulty level: E (Easy) || M (Medium) || H (Hard) \n")
 
-    print(HANGMANPICS[0])
-
-    # Used these to control the printshow_update function later on
-    easy = False
-    medium = False
-    hard = False
-
-    guesses_list = []
-
-    # Picks which word list to use based on the user's choice level 
-    if level == "easy":
-        current_pick = random.choice(easy_word_list).lower()
-        show_update = ["_ ", "_ ", "_"]
-        printshow_update(3, show_update)
-        easy = True
-
-    if level == "medium":
-        current_pick = random.choice(medium_word_list).lower()
-        show_update = ["_ ", "_ ", "_ ", "_ ", "_ "]
-        printshow_update(5, show_update)
-        medium = True
-
-    if level == "hard":
-        current_pick = random.choice(hard_word_list).lower()
-        show_update = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_"]
-        printshow_update(7, show_update)
-        hard = True
-    
-    # used this to control the following while function as well as to 
-    # know if the user has won or lost.
-    count = 0
-    tries = 6
-
-    print('\nGuess a letter: ', end="")
-    
-    # Get's the length of the current word pick. I used this to control
-    # the following while loop and other conditional statement that depends
-    # on the word size
-    check_len = len(current_pick)
-    
-    # Loop function that makes the game repetitive until win or lose
-    while (tries > 0) and not (count == check_len):
-        update_num = 0
+    # show_update is the array that contains the users guesses
+    # this function reduces ambiguity caused by constant printing of
+    # the show_update array in different ways.
+    def printshow_update(number, functn, text = "What could this word be: "):
+        print(text, end="")
+        for num in range(number):
+            print(functn[num], end=" ")
         
-        # Used this to get the input and also fix the issue of user using
-        # uppercase during the game
-        user_guess = input().lower()
 
-        mini_count = 0
-        should_break = False
+    # Play function
+    def play(level):
 
-        # To check if the user guessed a letter they had already guessed
-        if user_guess not in guesses_list:
-            guesses_list.append(user_guess)
-        else:
-            print("\nYou've already guessed this letter.\nGuess another!\n")
-            should_break = True
+        print(HANGMANPICS[0])
 
-
-        for serial in current_pick:
-
-            if should_break == True:
-                continue
-                         
-            elif user_guess == serial:
-                show_update[update_num] = serial
-                mini_count += 1
-
-            update_num += 1
-
-        count += mini_count
+        global final_score
         
-        if should_break == False:
-            if easy == True:
-                printshow_update(3, show_update, "-------> ")
-            if medium == True:
-                printshow_update(5, show_update, "-------> ")
-            if hard == True:
-                printshow_update(7, show_update, "-------> ")
+        if level == "easy":
+            score = 100
+            print(f"Current score = {final_score}")
+            print("Max score after each word guess: 100")
+            print("\n************* Start!! *************\n")
+        elif level == "medium":
+            score = 150
+            print(f"Current score = {final_score}")
+            print("Max score after each word guess: 150")
+            print("\n************* Start!! *************\n")
+        elif level == "hard":
+            score = 200
+            print(f"Current score = {final_score}")
+            print("Max score after each word guess: 200")
+            print("\n************* Start!! *************\n")
 
-            if mini_count > 0:
-                if count == check_len:
-                    print("\n\n************* You win!! *************")
-                else:
-                    print("\n\nWoaw... You're on fire!!")
-                    print("\nExecution paused :)")
-                    print(HANGMANPICS[6 - tries])
+        # Used these to control the printshow_update function later on
+        easy = False
+        medium = False
+        hard = False
+
+        guesses_list = []
+
+        # Picks which word list to use based on the user's choice level 
+        if level == "easy":
+            current_pick = random.choice(easy_word_list).lower()
+            show_update = ["_ ", "_ ", "_"]
+            printshow_update(3, show_update)
+            easy = True
+
+        if level == "medium":
+            current_pick = random.choice(medium_word_list).lower()
+            show_update = ["_ ", "_ ", "_ ", "_ ", "_ "]
+            printshow_update(5, show_update)
+            medium = True
+
+        if level == "hard":
+            current_pick = random.choice(hard_word_list).lower()
+            show_update = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_"]
+            printshow_update(7, show_update)
+            hard = True
+        
+        # used this to control the following while function as well as to 
+        # know if the user has won or lost.
+        count = 0
+        tries = 6
+
+        print('\nGuess a letter: ', end="")
+        
+        # Get's the length of the current word pick. I used this to control
+        # the following while loop and other conditional statement that depends
+        # on the word size
+        check_len = len(current_pick)
+        
+        # Loop function that makes the game repetitive until win or lose
+        while (tries > 0) and not (count == check_len):
+            update_num = 0
+            
+            # Used this to get the input and also fix the issue of user using
+            # uppercase during the game
+            user_guess = input().lower()
+
+            mini_count = 0
+            should_break = False
+
+            # To check if the user guessed a letter they had already guessed
+            if user_guess not in guesses_list:
+                guesses_list.append(user_guess)
             else:
-                tries -= 1
-                print("\n\nOops... that letter isn't in the word")
-                print(HANGMANPICS[6 - tries])
-                print(f"************* {tries} tries remaining *************")
-                if tries == 0:
-                    print(f"\nCorrect Word = {current_pick}")
-                    print("\n************* You lose!! *************\n")
-        
-        if tries > 0 and not count == check_len:
-            print("\nGuess another letter: ", end="")
-        
+                print("\nYou've already guessed this letter!\n")
+                should_break = True
 
-# Block of code that handles game's difficulty level
-if level_difficulty.lower() == "hard":
-    print("\nDifficulty level set to Hard!\nYou get to guess 7 letter words in 6 trials\n")
-    play("hard")
-elif level_difficulty.lower() == "medium":
-    print("\nDifficulty level set to Medium!\nYou get to guess 5 letter words in 6 trials\n")
-    play("medium")
-elif level_difficulty.lower() == "easy":
-    print("\nDifficulty level set to Easy!\nYou get to guess 3 letter words in 6 trials\n")
-    play("easy")
-else:
-    print("Please choose an option between \"Easy, Medium or Hard\"\n")
 
+            for serial in current_pick:
+
+                if should_break == True:
+                    continue
+                            
+                elif user_guess == serial:
+                    show_update[update_num] = serial
+                    mini_count += 1
+
+                update_num += 1
+
+            count += mini_count
+            
+            if should_break == False:
+                if easy == True:
+                    printshow_update(3, show_update, "-------> ")
+                if medium == True:
+                    printshow_update(5, show_update, "-------> ")
+                if hard == True:
+                    printshow_update(7, show_update, "-------> ")
+
+                if mini_count > 0:
+                    if count == check_len:
+                        print("\n\n************* You win!! *************")
+                        print(f"********** Score = {score} points *************\n")
+                        final_score += score
+                        print("Do you want to try again? (y/n): ", end="")
+                        another_try = input().lower()
+                        if another_try == 'y':
+                            play_game()
+                        elif another_try == 'n':
+                            print("\n************* Game Over!! *************")
+                            print(f"******* Final Score = {final_score} points *****\n")
+                        else: 
+                            print("\n************* Wrong Input!! *************\n")
+                            print("\n************** Game Over!! *************")
+                            print(f"***** Final Score = {final_score} points *****\n")
+                    else:
+                        print("\n\nWoaw... You're on fire!!")
+                        print("\nExecution paused :)")
+                        print(HANGMANPICS[6 - tries])
+                else:
+                    tries -= 1
+                    print("\n\nOops... that letter isn't in the word")
+                    print(HANGMANPICS[6 - tries])
+                    score -= 10
+                    if tries > 0:
+                        print(f"***** -10 points: Expected score after word guess = {score} ****")
+                    print(f"************* {tries} tries remaining *************")
+                    if tries == 0:
+                        print(f"\nCorrect Word = {current_pick}")
+                        print("\n************* You lose!! *************")
+                        print(f"*********** Score = 0 points ***********\n")
+                        print("Do you want to try again? (y/n): ", end="")
+                        another_try = input().lower()
+                        if another_try == 'y':
+                            play_game()
+                        elif another_try == 'n':
+                            print("\n************* Game Over!! *************")
+                            print(f"***** Final Score = {final_score} points *****\n")
+                        else: 
+                            print("\n************* Wrong Input!! *************\n")
+                            print("\n************** Game Over!! *************")
+                            print(f"***** Final Score = {final_score} points *****\n")
+            
+            if tries > 0 and not count == check_len:
+                print("\nGuess another letter: ", end="")
+            
+            
+    
+
+
+
+    # Block of code that handles game's difficulty level
+    if level_difficulty.lower() == "h":
+        print("\nDifficulty level set to Hard!\nYou get to guess 7 letter words in 6 trials\n")
+        play("hard")
+    elif level_difficulty.lower() == "m":
+        print("\nDifficulty level set to Medium!\nYou get to guess 5 letter words in 6 trials\n")
+        play("medium")
+    elif level_difficulty.lower() == "e":
+        print("\nDifficulty level set to Easy!\nYou get to guess 3 letter words in 6 trials\n")
+        play("easy")
+    else:
+        print("Please choose an option from the list", end="")
+        play_game()
+
+play_game()
